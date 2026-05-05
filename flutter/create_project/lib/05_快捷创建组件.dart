b68c2163677f2bd88b7b2f74791e5901bd5c2269
isonground <258521847+isonground@users.noreply.github.com>
2026-05-05 22:30:55 +0800

    5/5

---
+++ flutter/create_project/lib/05_快捷创建组件.dart
@@ -0,0 +1,37 @@
+// 快捷创建组件
+// 在安装awesome flutter snippets后，即可通过命令快速创建组件
+// 快速创建无状态组件：statelessw
+// 快速创建有状态组件：statefulw
+// 这里同时要记得引入头文件`import 'package:flutter/material.dart';`
+// 可以通过快速修复功能自动引入头文件引用
+
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  
+}
+
+// // statelessw 回车
+// class name extends StatelessWidget {
+//   const name({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container();
+//   }
+// }
+
+// // statefulw 回车
+// class name extends StatefulWidget {
+//   const name({super.key});
+
+//   @override
+//   State<name> createState() => _nameState();
+// }
+
+// class _nameState extends State<name> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container();
+//   }
+// }
